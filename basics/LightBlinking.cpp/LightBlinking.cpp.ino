abf4a7e19f70a2145f515713d294dc4acd7f4b6c
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2023-02-27 00:28:37 +0530

    blinking of light

---
+++ basics/LightBlinking.cpp/LightBlinking.cpp.ino
@@ -0,0 +1,13 @@
+// LED_BUILTIN is built in LED connected to digital pin 13.
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
